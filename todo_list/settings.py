e639bff7bb865a8711cc966efcbaeae51a2ec285
Sungju Kim <starararara@naver.com>
2023-06-02 00:08:17 +0900

    second-commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t$(*32yq8lqt%=)_nrb0t!w$g3-98t8pt)s#w#*6o)leo6vbk)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kiddo-to-do-list.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
