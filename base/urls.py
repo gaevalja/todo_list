fe73a14c87e2d2ea5d8fd1cbbedc68cdf9bfe0bc
Sungju Kim <starararara@naver.com>
2023-06-01 23:04:58 +0900

    first-commit

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register')
+]
